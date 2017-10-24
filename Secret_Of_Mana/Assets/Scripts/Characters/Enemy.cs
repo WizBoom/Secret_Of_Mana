c2b6165734e29eb01b0192c1fbc66a0f71007e0a
WizBoom <brian.van.hyfte@gmail.com>
2017-10-24 20:11:55 +0200

    Move CreateAssetMenu from Character to Enemy & Player

--- Secret_Of_Mana/Assets/Scripts/Characters/Enemy.cs
+++ Secret_Of_Mana/Assets/Scripts/Characters/Enemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Character", menuName = "Character/Enemy")]
 public class Enemy : Character
 {
 
--- Secret_Of_Mana/Assets/Scripts/Characters/Player.cs
+++ Secret_Of_Mana/Assets/Scripts/Characters/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Character", menuName = "Character/Player")]
 public class Player : Character
 {
 
