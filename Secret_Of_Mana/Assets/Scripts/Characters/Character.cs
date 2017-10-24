eb4f18cf127ec8a4b763fc39cb9ba968876d7f52
WizBoom <brian.van.hyfte@gmail.com>
2017-10-24 20:02:12 +0200

    Create basic character / character behaviour setup

--- Secret_Of_Mana/Assets/Scripts/Characters/Character.cs
+++ Secret_Of_Mana/Assets/Scripts/Characters/Character.cs
@@ -1,8 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Character", menuName = "Character/Character")]
 public class Character : ScriptableObject
 {
+    public String m_CharacterName;
+    public int m_MaxHealthPoints;
+    private int _CurrentHealthPoints;
+    public int m_MaxManaPoints;
+    private int _CurrentManaPoints;
+    public Weapon m_Weapon;
+    private int _Level;
+    public int m_Attack;
+    public int m_Defence;
 
+    public CharacterBehaviour m_CharacterBehaviour;
+    public VisualCharacter m_VisualCharacter;
 }
---
+++ Secret_Of_Mana/Assets/Scripts/Characters/CharacterBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Abstract class that defines a character's behaviour
+public abstract class CharacterBehaviour : ScriptableObject
+{
+    public abstract void Update();
+}
