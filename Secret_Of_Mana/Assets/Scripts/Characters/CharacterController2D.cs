967692f5411cba78f703efdf3c04bc17b2624f85
WizBoom <brian.van.hyfte@gmail.com>
2017-10-25 20:03:51 +0200

    Put RigidBody in private variable instead of calling GetComponent in update

--- Secret_Of_Mana/Assets/Scripts/Characters/CharacterController2D.cs
+++ Secret_Of_Mana/Assets/Scripts/Characters/CharacterController2D.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController2D : MonoBehaviour
 {
     public int m_Speed { get; set; }
+    private Rigidbody2D _RigidBody;
+
+    void Awake()
+    {
+        _RigidBody = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         if (!Mathf.Approximately(Input.GetAxisRaw("Horizontal"), 0f) || 
             !Mathf.Approximately(Input.GetAxisRaw("Vertical"), 0f))
         {
-            //Vector2 translation = Vector2.right * m_Character.m_Speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-            //transform.Translate(new Vector3(translation.x, translation.y, 0));
-            GetComponent<Rigidbody2D>().velocity = new Vector2(m_Speed * Input.GetAxisRaw("Horizontal"),
+            _RigidBody.velocity = new Vector2(m_Speed * Input.GetAxisRaw("Horizontal"),
                m_Speed * Input.GetAxisRaw("Vertical"));
         }
         else
