d54f614f96955c39e8ddb8ef8ebec811059d95bb
Brian Van Hyfte <brian.van.hyfte@student.howest.be>
2017-10-28 14:30:40 +0200

    Add basic staff heal

--- Secret_Of_Mana/Assets/Scripts/Items/Staff.cs
+++ Secret_Of_Mana/Assets/Scripts/Items/Staff.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Staff", menuName = "Items/Weapons/Staff")]
 public class Staff : Weapon
 {
     public override void Attack(Direction direction, VisualCharacter visualCharacter)
     {
+        Debug.Log("Healing...");
+
+        //Calculate the range based on the size of the character
+        Vector2 range = new Vector2(visualCharacter.GetComponent<BoxCollider2D>().size.x + m_Range,
+            visualCharacter.GetComponent<BoxCollider2D>().size.y + m_Range);
+
+        Collider2D[] colliders = Physics2D.OverlapCapsuleAll(visualCharacter.transform.position, range, CapsuleDirection2D.Vertical, 0f);
+        foreach (var collider in colliders)
+        {
+            if (collider != visualCharacter.GetComponent<Collider2D>())
+                Debug.Log(collider.gameObject.name);
+        }
     }
 }
