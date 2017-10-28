d7b97989ce2b1fe4ba4129bbdfd2af2a0140f4f9
Brian Van Hyfte <brian.van.hyfte@student.howest.be>
2017-10-28 16:22:14 +0200

    Add grass sprite & link to sources

--- Secret_Of_Mana/Assets/Scripts/Items/Staff.cs
+++ Secret_Of_Mana/Assets/Scripts/Items/Staff.cs
@@ -11,22 +11,23 @@ public class Staff : Weapon
     public override void Attack(Direction direction, VisualCharacter visualCharacter)
     {
         Debug.Log("Healing...");
-
-        //Calculate the range based on the size of the character
-        Vector2 range = new Vector2(visualCharacter.GetComponent<BoxCollider2D>().size.x + m_Range,
+        if (visualCharacter.ApplyManapoints(-m_ManaConsumption))
+        {
+            //Calculate the range based on the size of the character
+            Vector2 range = new Vector2(visualCharacter.GetComponent<BoxCollider2D>().size.x + m_Range,
             visualCharacter.GetComponent<BoxCollider2D>().size.y + m_Range);
 
-        Collider2D[] colliders = Physics2D.OverlapCapsuleAll(visualCharacter.transform.position, range, CapsuleDirection2D.Vertical, 0f);
-        foreach (var collider in colliders)
-        {
-            if (collider != visualCharacter.GetComponent<Collider2D>() && collider.tag == "Character")
+            Collider2D[] colliders = Physics2D.OverlapCapsuleAll(visualCharacter.transform.position, range, CapsuleDirection2D.Vertical, 0f);
+            foreach (var collider in colliders)
             {
-                //Get character
-                VisualCharacter character = collider.GetComponent<VisualCharacter>();
-                if (character)
+                if (collider != visualCharacter.GetComponent<Collider2D>() && collider.tag == "Character")
                 {
-                    if (character.ApplyManapoints(-m_ManaConsumption))
+                    //Get character
+                    VisualCharacter character = collider.GetComponent<VisualCharacter>();
+                    if (character)
+                    {
                         character.ApplyHealthpoints(m_HealAmount);
+                    }
                 }
             }
         }
