609308abd57d79d6988b3f6dc84d3ffb3ce5490e
Brian Van Hyfte <brian.van.hyfte@student.howest.be>
2017-10-29 22:32:29 +0100

    Generate buttons for character panel

--- Secret_Of_Mana/Assets/Scripts/Managers/GameManager.cs
+++ Secret_Of_Mana/Assets/Scripts/Managers/GameManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,6 +20,7 @@ public class GameManager : MonoBehaviour
         m_UIManager.Init();
 
         CreateCharacterPrefabs();
+        CreateCharacterPanel();
     }
 
     private void Update()
@@ -54,12 +57,13 @@ public class GameManager : MonoBehaviour
                 VisualCharacter character = Instantiate(player.m_VisualCharacter, player.m_LastSavedPosition,
                     Quaternion.identity);
                 //Add character controller, but disable it
-                if (!character.gameObject.GetComponent<CharacterController2D>())
+                var controller = character.gameObject.GetComponent<CharacterController2D>();
+                if (!controller)
                 {
-                    var controller = character.gameObject.AddComponent<CharacterController2D>();
+                    controller = character.gameObject.AddComponent<CharacterController2D>();
                     controller.m_Speed = player.m_CharacterStats.m_Speed;
-                    controller.enabled = false;
                 }
+                controller.enabled = false;
                 player.m_VisualCharacter = character;
                 character.m_Character = player;
                 player.InitCharacter();
@@ -72,5 +76,62 @@ public class GameManager : MonoBehaviour
             m_CharacterManager.m_CurrentPlayer.m_VisualCharacter.GetComponent<CharacterController2D>().enabled = true;
             Camera.main.transform.parent = m_CharacterManager.m_CurrentPlayer.m_VisualCharacter.transform;
         }
+    }
+
+    private void CreateCharacterPanel()
+    {
+        //Set content height
+        float height = (m_CharacterManager.m_Players.Count*m_UIManager.m_ButtonHeight) + ((m_CharacterManager.m_Players.Count + 1)*m_UIManager.m_ButtonDistance);
+        m_UIManager.m_ContentObject.sizeDelta = new Vector2(m_UIManager.m_ContentObject.sizeDelta.x,height);
+
+        //Set start position
+        float yPos = m_UIManager.m_ButtonHeight / 2f;
+
+        //Create button for every character
+        for (int i = 0; i < m_CharacterManager.m_Players.Count; i++)
+        {
+            var player = m_CharacterManager.m_Players[i];
+
+            yPos -= m_UIManager.m_ButtonHeight + m_UIManager.m_ButtonDistance;
+
+            //Button
+            GameObject button = new GameObject(m_CharacterManager.m_Players[i].m_CharacterStats.m_CharacterName);
+            button.transform.parent = m_UIManager.m_ContentObject;
+            var rectTransform = button.AddComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0, 1);
+            rectTransform.anchorMax = new Vector2(1, 1);
+            rectTransform.offsetMin = new Vector2(m_UIManager.m_ButtonMargin, yPos - (m_UIManager.m_ButtonHeight / 2f));
+            rectTransform.offsetMax = new Vector2(-m_UIManager.m_ButtonMargin, yPos + (m_UIManager.m_ButtonHeight / 2f));
+
+            button.AddComponent<CanvasRenderer>();
+            var imageComponent = button.AddComponent<Image>();
+            imageComponent.sprite = m_UIManager.m_ButtonSprite;
+            imageComponent.type = Image.Type.Sliced;
+            imageComponent.color = m_UIManager.m_ButtonColor;
+            var buttonComponent = button.AddComponent<Button>();
+            int charIndex = i;
+            buttonComponent.onClick.AddListener(delegate { TempFunction(charIndex); });
+
+            //Text
+            var textObject = new GameObject("Text");
+            textObject.transform.parent = button.transform;
+            rectTransform = textObject.AddComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0, 0);
+            rectTransform.anchorMax = new Vector2(1, 1);
+            rectTransform.offsetMin = new Vector2(0, 0);
+            rectTransform.offsetMax = new Vector2(0, 0);
+            var textComponent = textObject.AddComponent<Text>();
+            textComponent.text = player.m_CharacterStats.m_CharacterName;
+            textComponent.font = m_UIManager.m_ButtonTextFont;
+            textComponent.fontSize = m_UIManager.m_ButtonTextSize;
+            textComponent.color = m_UIManager.m_ButtonTextColor;
+            textComponent.alignment = TextAnchor.MiddleCenter;
+        }
+    }
+
+    //TODO: CharacterSwitch
+    public void TempFunction(int a)
+    {
+        Debug.Log(a);
     }
 }
--- Secret_Of_Mana/Assets/Scripts/Managers/UIManager.cs
+++ Secret_Of_Mana/Assets/Scripts/Managers/UIManager.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class UIManager
 {
     public InventoryPanel m_InventoryPanel;
     public CharacterPanel m_CharacterPanel;
+    public RectTransform m_ContentObject;
+    public float m_ButtonHeight = 50f;
+    public float m_ButtonMargin = 10f;
+    public float m_ButtonDistance = 10f;
+    public Sprite m_ButtonSprite;
+    public Color m_ButtonColor;
+    public int m_ButtonTextSize = 14;
+    public Font m_ButtonTextFont;
+    public Color m_ButtonTextColor;
 
     public void Init()
     {
