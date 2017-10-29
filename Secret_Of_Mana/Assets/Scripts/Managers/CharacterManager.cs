4bf44c676b27b1a44836cd60f7a6373f69f01d19
Brian Van Hyfte <brian.van.hyfte@student.howest.be>
2017-10-29 13:39:12 +0100

    Move prefab creation to game manager

--- Secret_Of_Mana/Assets/Scripts/Managers/CharacterManager.cs
+++ Secret_Of_Mana/Assets/Scripts/Managers/CharacterManager.cs
@@ -9,46 +9,4 @@ public class CharacterManager
     public List<Player> m_Players;
     [HideInInspector]
     public Player m_CurrentPlayer;
-
-    public void Init()
-    {
-        //Make Enemies
-        foreach (var enemy in m_Enemies)
-        {
-            if (enemy.m_VisualCharacter)
-            {
-                VisualCharacter character = VisualCharacter.Instantiate(enemy.m_VisualCharacter, enemy.m_LastSavedPosition,
-                    Quaternion.identity) as VisualCharacter;
-                enemy.m_VisualCharacter = character;
-                character.m_Character = enemy;
-                enemy.InitCharacter();
-            }
-        }
-
-        foreach (var player in m_Players)
-        {
-            if (player.m_VisualCharacter)
-            {
-                VisualCharacter character = VisualCharacter.Instantiate(player.m_VisualCharacter, player.m_LastSavedPosition,
-                    Quaternion.identity);
-                //Add character controller, but disable it
-                if (!character.gameObject.GetComponent<CharacterController2D>())
-                {
-                    var controller = character.gameObject.AddComponent<CharacterController2D>();
-                    controller.m_Speed = player.m_CharacterStats.m_Speed;
-                    controller.enabled = false;
-                }
-                player.m_VisualCharacter = character;
-                character.m_Character = player;
-                player.InitCharacter();
-            }
-        }
-
-        if (m_Players.Count > 0)
-        {
-            m_CurrentPlayer = m_Players[0];
-            m_CurrentPlayer.m_VisualCharacter.GetComponent<CharacterController2D>().enabled = true;
-            Camera.main.transform.parent = m_CurrentPlayer.m_VisualCharacter.transform;
-        }
-    }
 }
--- Secret_Of_Mana/Assets/Scripts/Managers/GameManager.cs
+++ Secret_Of_Mana/Assets/Scripts/Managers/GameManager.cs
@@ -13,6 +13,48 @@ public class GameManager : MonoBehaviour
         if (!m_Instance)
             m_Instance = this;
 
-        m_CharacterManager.Init();
+        CreateCharacterPrefabs();
+    }
+
+    private void CreateCharacterPrefabs()
+    {
+        //Make Enemies
+        foreach (var enemy in m_CharacterManager.m_Enemies)
+        {
+            if (enemy.m_VisualCharacter)
+            {
+                VisualCharacter character = Instantiate(enemy.m_VisualCharacter, enemy.m_LastSavedPosition,
+                    Quaternion.identity);
+                enemy.m_VisualCharacter = character;
+                character.m_Character = enemy;
+                enemy.InitCharacter();
+            }
+        }
+
+        foreach (var player in m_CharacterManager.m_Players)
+        {
+            if (player.m_VisualCharacter)
+            {
+                VisualCharacter character = Instantiate(player.m_VisualCharacter, player.m_LastSavedPosition,
+                    Quaternion.identity);
+                //Add character controller, but disable it
+                if (!character.gameObject.GetComponent<CharacterController2D>())
+                {
+                    var controller = character.gameObject.AddComponent<CharacterController2D>();
+                    controller.m_Speed = player.m_CharacterStats.m_Speed;
+                    controller.enabled = false;
+                }
+                player.m_VisualCharacter = character;
+                character.m_Character = player;
+                player.InitCharacter();
+            }
+        }
+
+        if (m_CharacterManager.m_Players.Count > 0)
+        {
+            m_CharacterManager.m_CurrentPlayer = m_CharacterManager.m_Players[0];
+            m_CharacterManager.m_CurrentPlayer.m_VisualCharacter.GetComponent<CharacterController2D>().enabled = true;
+            Camera.main.transform.parent = m_CharacterManager.m_CurrentPlayer.m_VisualCharacter.transform;
+        }
     }
 }
